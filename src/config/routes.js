a8e86ecf21edc2a0dbbb0badb80c2e30a82f3bee
Elene Dgebuadze <elenedgebuadze83@gmail.com>
2024-04-04 19:48:07 +0400

    Define file structure

---
+++ src/config/routes.js
@@ -0,0 +1,9 @@
+import { routes } from "../constants/routes";
+import Home from "../pages/home/Home";
+
+export const appRoutes = [
+  {
+    path: routes.home,
+    Component: Home,
+  },
+];
---
+++ src/constants/routes.js
@@ -0,0 +1,7 @@
+const home = "/";
+const flights = "/flights";
+const booking = "/booking";
+const signIn = "/signIn";
+const signUp = "/signUp";
+
+export const routes = { home, flights, booking, signIn, signUp };
