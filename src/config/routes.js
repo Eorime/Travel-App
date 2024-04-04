c195243d2a063154ce37678f54d546ebf06a1ff1
Elene Dgebuadze <elenedgebuadze83@gmail.com>
2024-04-04 20:58:22 +0400

    Add pages

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,9 +1,34 @@
 import { routes } from "../constants/routes";
 import Home from "../pages/home/Home";
+import Services from "../pages/services/Services";
+import Flights from "../pages/flights/Flights";
+import Stays from "../pages/stays/Stays";
+import SignIn from "../pages/signIn/SignIn";
+import SignUp from "../pages/signUp/SignUp";
 
 export const appRoutes = [
   {
     path: routes.home,
     Component: Home,
+  },
+  {
+    path: routes.services,
+    Component: Services,
+  },
+  {
+    path: routes.flights,
+    Component: Flights,
+  },
+  {
+    path: routes.stays,
+    Component: Stays,
+  },
+  {
+    path: routes.signIn,
+    Component: SignIn,
+  },
+  {
+    path: routes.signUp,
+    Component: SignUp,
   },
 ];
--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,7 +1,19 @@
 const home = "/";
+const services = "/services";
 const flights = "/flights";
-const booking = "/booking";
+const stays = "/stays";
+const staysSearch = "/staysSearch";
+const staysDetails = "/staysDetails";
 const signIn = "/signIn";
 const signUp = "/signUp";
 
-export const routes = { home, flights, booking, signIn, signUp };
+export const routes = {
+  home,
+  flights,
+  services,
+  stays,
+  staysSearch,
+  staysDetails,
+  signIn,
+  signUp,
+};
