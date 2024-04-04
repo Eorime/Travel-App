b9d3787ceb0f0246856c76c9f92e2a53c99a5c09
Elene Dgebuadze <elenedgebuadze83@gmail.com>
2024-04-05 00:44:25 +0400

    Add discover services

--- src/components/discoverServices/DiscoverServices.jsx
+++ src/components/discoverServices/DiscoverServices.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import {
+  DiscoverContainer,
+  DiscoverElement,
+  DiscoverElements,
+  Title,
+} from "./style";
 
 const DiscoverServices = () => {
-  return <div>DiscoverServices</div>;
+  return (
+    <DiscoverContainer>
+      <Title>Discover Our Services</Title>
+      <DiscoverElements>
+        <DiscoverElement></DiscoverElement>
+      </DiscoverElements>
+    </DiscoverContainer>
+  );
 };
 
 export default DiscoverServices;
--- src/components/discoverServices/style.js
+++ src/components/discoverServices/style.js
@@ -1,1 +1,15 @@
 import styled from "styled-components";
+
+export const DiscoverContainer = styled.div``;
+
+export const Title = styled.h`
+  font-size: 40px;
+`;
+
+export const Paragraph = styled.p``;
+
+export const DiscoverElements = styled.div``;
+
+export const DiscoverElement = styled.div``;
+
+export const Description = styled.p``;
