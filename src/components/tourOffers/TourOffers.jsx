a9a3d8cb04828fd3d77aa99b902a080fba200a52
Elene Dgebuadze <elenedgebuadze83@gmail.com>
2024-04-05 20:27:44 +0400

    Set up homepage components

---
+++ src/components/tourOffers/TourOffers.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TourOffers = () => {
+  return <div>TourOffers</div>;
+};
+
+export default TourOffers;
---
+++ src/components/watchTrips/WatchTrips.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WatchTrips = () => {
+  return <div>WatchTrips</div>;
+};
+
+export default WatchTrips;
