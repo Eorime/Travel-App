f87c492969b914b750d0b4e9955ba2221131b55d
Elene Dgebuadze <elenedgebuadze83@gmail.com>
2024-04-07 01:31:57 +0400

    Add header and footer to the services component

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+import { Container } from "./style";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <>
+      <Header />
+      <Container></Container>
+      <Footer />
+    </>
+  );
 };
 
 export default Services;
