8ef9a4e44df4f07d8bd874dd09d81cee5b31471e
Elene Dgebuadze <elenedgebuadze83@gmail.com>
2024-04-08 18:48:05 +0400

    Add buttons in flight filter

--- src/components/flightsFilter/FlightsFilter.jsx
+++ src/components/flightsFilter/FlightsFilter.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   AirlineName,
   Container,
@@ -14,22 +14,38 @@ import {
 import { flightsData } from "../../data/flightsData/data";
 
 const FlightsFilter = () => {
-  const showMore = () => {};
+  const [showFlights, setShowFlights] = useState(false);
+  const firstFlights = 5;
+
+  const showMore = () => {
+    setShowFlights(true);
+  };
+
+  const showLess = () => {
+    setShowFlights(false);
+  };
+
   return (
     <Container>
       <FlightsContainer>
-        {flightsData.map((flight, index) => (
-          <Flight key={index}>
-            <FlightLogo src={flight.img} />
-            <AirlineName>{flight.airlinesName}</AirlineName>
-            <FlightTakeOff>Take off {flight.takeOff}</FlightTakeOff>
-            <FlightLanding>Landing {flight.landing}</FlightLanding>
-            <FlightHours>{flight.hours}</FlightHours>
-            <FlightPrice>{flight.price}</FlightPrice>
-          </Flight>
-        ))}
+        {flightsData
+          .slice(0, showFlights ? undefined : firstFlights)
+          .map((flight, index) => (
+            <Flight key={index}>
+              <FlightLogo src={flight.img} />
+              <AirlineName>{flight.airlinesName}</AirlineName>
+              <FlightTakeOff>Take off {flight.takeOff}</FlightTakeOff>
+              <FlightLanding>Landing {flight.landing}</FlightLanding>
+              <FlightHours>{flight.hours}</FlightHours>
+              <FlightPrice>{flight.price}</FlightPrice>
+            </Flight>
+          ))}
       </FlightsContainer>
-      <SeeMoreButton onClick={showMore}>Show more results</SeeMoreButton>
+      {showFlights ? (
+        <SeeMoreButton onClick={showLess}>Show less results</SeeMoreButton>
+      ) : (
+        <SeeMoreButton onClick={showMore}>Show more results</SeeMoreButton>
+      )}
     </Container>
   );
 };
---
+++ src/data/staysData/data.js
@@ -0,0 +1,144 @@
+import Zealand from "../../assets/images/zealand.png";
+
+export const staysData = [
+  {
+    img: Zealand,
+    title: "New Zealand Resort",
+    subTitle: "New Zealand, Auckland",
+    locationText: "1 km away from center",
+    description:
+      "Near beach, with private beach area and private pool. Property has terrace, kitchen, 2 bathrooms",
+    rating: 5,
+    price: "400$",
+  },
+  {
+    img: Zealand,
+    title: "New Zealand Resort",
+    subTitle: "New Zealand, Auckland",
+    locationText: "1 km away from center",
+    description:
+      "Near beach, with private beach area and private pool. Property has terrace, kitchen, 2 bathrooms",
+    rating: 3,
+    price: "400$",
+  },
+  {
+    img: Zealand,
+    title: "New Zealand Resort",
+    subTitle: "New Zealand, Auckland",
+    locationText: "1 km away from center",
+    description:
+      "Near beach, with private beach area and private pool. Property has terrace, kitchen, 2 bathrooms",
+    rating: 4,
+    price: "400$",
+  },
+  {
+    img: Zealand,
+    title: "New Zealand Resort",
+    subTitle: "New Zealand, Auckland",
+    locationText: "1 km away from center",
+    description:
+      "Near beach, with private beach area and private pool. Property has terrace, kitchen, 2 bathrooms",
+    rating: 2,
+    price: "400$",
+  },
+  {
+    img: Zealand,
+    title: "New Zealand Resort",
+    subTitle: "New Zealand, Auckland",
+    locationText: "1 km away from center",
+    description:
+      "Near beach, with private beach area and private pool. Property has terrace, kitchen, 2 bathrooms",
+    rating: 5,
+    price: "400$",
+  },
+  {
+    img: Zealand,
+    title: "New Zealand Resort",
+    subTitle: "New Zealand, Auckland",
+    locationText: "1 km away from center",
+    description:
+      "Near beach, with private beach area and private pool. Property has terrace, kitchen, 2 bathrooms",
+    rating: 4,
+    price: "400$",
+  },
+  {
+    img: Zealand,
+    title: "New Zealand Resort",
+    subTitle: "New Zealand, Auckland",
+    locationText: "1 km away from center",
+    description:
+      "Near beach, with private beach area and private pool. Property has terrace, kitchen, 2 bathrooms",
+    rating: 1,
+    price: "400$",
+  },
+  {
+    img: Zealand,
+    title: "New Zealand Resort",
+    subTitle: "New Zealand, Auckland",
+    locationText: "1 km away from center",
+    description:
+      "Near beach, with private beach area and private pool. Property has terrace, kitchen, 2 bathrooms",
+    rating: 5,
+    price: "400$",
+  },
+  {
+    img: Zealand,
+    title: "New Zealand Resort",
+    subTitle: "New Zealand, Auckland",
+    locationText: "1 km away from center",
+    description:
+      "Near beach, with private beach area and private pool. Property has terrace, kitchen, 2 bathrooms",
+    rating: 2,
+    price: "400$",
+  },
+  {
+    img: Zealand,
+    title: "New Zealand Resort",
+    subTitle: "New Zealand, Auckland",
+    locationText: "1 km away from center",
+    description:
+      "Near beach, with private beach area and private pool. Property has terrace, kitchen, 2 bathrooms",
+    rating: 3,
+    price: "400$",
+  },
+  {
+    img: Zealand,
+    title: "New Zealand Resort",
+    subTitle: "New Zealand, Auckland",
+    locationText: "1 km away from center",
+    description:
+      "Near beach, with private beach area and private pool. Property has terrace, kitchen, 2 bathrooms",
+    rating: 4,
+    price: "400$",
+  },
+  {
+    img: Zealand,
+    title: "New Zealand Resort",
+    subTitle: "New Zealand, Auckland",
+    locationText: "1 km away from center",
+    description:
+      "Near beach, with private beach area and private pool. Property has terrace, kitchen, 2 bathrooms",
+    rating: 2,
+    price: "400$",
+  },
+  {
+    img: Zealand,
+    title: "New Zealand Resort",
+    subTitle: "New Zealand, Auckland",
+    locationText: "1 km away from center",
+    description:
+      "Near beach, with private beach area and private pool. Property has terrace, kitchen, 2 bathrooms",
+    rating: 5,
+    price: "400$",
+  },
+  {
+    img: Zealand,
+    title: "New Zealand Resort",
+    subTitle: "New Zealand, Auckland",
+    locationText: "1 km away from center",
+    description:
+      "Near beach, with private beach area and private pool. Property has terrace, kitchen, 2 bathrooms",
+    rating: 3,
+    price: "400$",
+  },
+];
