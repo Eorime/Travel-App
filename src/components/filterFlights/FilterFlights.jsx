b88703e2bbab6ce5cdb88b5ff313c7b2788901b0
Elene Dgebuadze <elenedgebuadze83@gmail.com>
2024-04-10 16:52:52 +0400

    Working on flight filters

--- src/components/filterFlights/FilterFlights.jsx
+++ src/components/filterFlights/FilterFlights.jsx
@@ -1,7 +1,37 @@
-import React from "react";
-import { CheapestContainer, Container, Line, Line2, Paragraph } from "./style";
+import React, { useState } from "react";
+import {
+  CheapestContainer,
+  CheckboxButton,
+  CheckboxButtonImage,
+  Container,
+  FiltersContainer,
+  Line,
+  Line2,
+  Paragraph,
+  StopsCheckbox,
+  StopsCheckboxLabel,
+  StopsContainer,
+  StopsContent,
+  StopsTitle,
+  TimesContainer,
+  TimesTitle,
+} from "./style";
+import OpenForm from "../../assets/images/openform.png";
+import CloseForm from "../../assets/images/closeform.png";
 
 const FilterFlights = () => {
+  const [stopsDropDown, setStopsDropdown] = useState(false);
+  const [selectedStop, setSelectedStop] = useState(null);
+
+  const handleStops = () => {
+    setStopsDropdown(!stopsDropDown);
+  };
+
+  const handleStopChange = (event) => {
+    const { id } = event.target;
+    setSelectedStop(id === selectedStop ? null : id);
+  };
+
   return (
     <Container>
       <CheapestContainer>
@@ -11,6 +41,56 @@ const FilterFlights = () => {
         <Line2></Line2>
         <Paragraph>Best</Paragraph>
       </CheapestContainer>
+      <FiltersContainer>
+        <StopsContainer>
+          <StopsTitle>Stops</StopsTitle>
+          <CheckboxButton onClick={handleStops}>
+            {stopsDropDown ? (
+              <CheckboxButtonImage
+                src={CloseForm}
+                style={{ marginTop: "-43px" }}
+                alt="Close Button"
+              />
+            ) : (
+              <CheckboxButtonImage src={OpenForm} alt="Open Button" />
+            )}
+          </CheckboxButton>
+          {stopsDropDown && (
+            <StopsContent>
+              <StopsCheckboxLabel>
+                <StopsCheckbox
+                  type="checkbox"
+                  id="direct"
+                  checked={selectedStop === "direct"}
+                  onChange={handleStopChange}
+                />
+                <span>Direct</span>
+              </StopsCheckboxLabel>
+              <StopsCheckboxLabel>
+                <StopsCheckbox
+                  type="checkbox"
+                  id="twoStops"
+                  checked={selectedStop === "twoStops"}
+                  onChange={handleStopChange}
+                />
+                <span>Two stops</span>
+              </StopsCheckboxLabel>
+              <StopsCheckboxLabel>
+                <StopsCheckbox
+                  type="checkbox"
+                  id="threeStops"
+                  checked={selectedStop === "threeStops"}
+                  onChange={handleStopChange}
+                />
+                <span>Three stops</span>
+              </StopsCheckboxLabel>
+            </StopsContent>
+          )}
+        </StopsContainer>
+        <TimesContainer>
+          <TimesTitle>Times</TimesTitle>
+        </TimesContainer>
+      </FiltersContainer>
     </Container>
   );
 };
--- src/components/filterFlights/style.js
+++ src/components/filterFlights/style.js
@@ -38,3 +38,75 @@ export const Line2 = styled.div`
   margin-top: 160px;
   position: absolute;
 `;
+
+export const FiltersContainer = styled.div`
+  height: 1430px;
+  width: 370px;
+  margin-top: 260px;
+  background-color: #ffffff;
+  border-radius: 20px;
+  position: absolute;
+`;
+
+export const StopsTitle = styled.h1`
+  font-weight: 400;
+  font-size: 28px;
+  color: #424244;
+  margin-top: 20px;
+`;
+
+export const StopsContainer = styled.div`
+  margin-left: 20px;
+`;
+
+export const StopsContent = styled.div`
+  display: ${(props) => (props.open ? "block" : "none")};
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  margin-top: -30px;
+`;
+
+export const StopsCheckboxLabel = styled.label`
+  font-size: 24px;
+  color: #424244;
+`;
+
+export const StopsCheckbox = styled.input`
+  margin-bottom: 20px;
+  margin-right: 20px;
+  width: 30px;
+  height: 30px;
+
+  &: hover {
+    cursor: pointer;
+  }
+`;
+
+export const CheckboxButton = styled.div`
+  background-color: transparent;
+  border: none;
+  width: 40px;
+  height: 40px;
+
+  &: hover {
+    cursor: pointer;
+  }
+`;
+
+export const CheckboxButtonImage = styled.img`
+  position: absolute;
+  margin-left: 280px;
+  margin-top: -50px;
+`;
+
+export const TimesContainer = styled.div`
+  margin-left: 20px;
+`;
+
+export const TimesTitle = styled.h1`
+  font-weight: 400;
+  font-size: 28px;
+  color: #424244;
+  margin-top: 130px;
+`;
